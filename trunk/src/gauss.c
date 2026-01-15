f700e7d0ea775b4173fd2d3c412730d9b69075f6
mikolaj-nierodzinski <01206800@pw.edu.pl>
2026-01-15 22:47:53 +0100

    modified gauss implementation

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -9,13 +9,32 @@ int eliminate(Matrix *mat, Matrix *b) {
     int n = mat->r;
     int i, j, k;
     double factor;
-
+	
     for (k = 0; k < n - 1; k++) {
-        
-        if (mat->data[k][k]==1) {
+	//Wybór elementu głównego
+    int maksw = k;
+    double maks = fabs(mat->data[k][k]);
+    for (int l = k + 1; l < n; l++) {
+        if (fabs(mat->data[l][k]) > maks) {
+            maks = fabs(mat->data[l][k]);
+            maksw = l;
+        }
+    }
+
+    //Zamiana wierszy
+    if (maksw != k) {
+        double *tempRow = mat->data[k];
+        mat->data[k] = mat->data[maksw];
+        mat->data[maksw] = tempRow;
+
+        double tempB = b->data[k][0];
+        b->data[k][0] = b->data[maksw][0];
+        b->data[maksw][0] = tempB;
+    }
+        if (fabs(mat->data[k][k])<0.00001) {
             return 1;
         }
-
+	//eliminacja
         for (i = k + 1; i < n; i++) {
             factor = mat->data[i][k] / mat->data[k][k];
 
